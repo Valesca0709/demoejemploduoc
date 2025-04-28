fd6e63c9b4bb37df60e67d189b3a66612bb3cbee
Valesca0709 <valesca.torres07@gmail.com>
2025-04-28 19:15:06 -0400

    Initial commit

---
+++ src/main/java/com/ejemplo/demoejemploduoc/DemoejemploduocApplication.java
@@ -0,0 +1,13 @@
+package com.ejemplo.demoejemploduoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoejemploduocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoejemploduocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ejemplo/demoejemploduoc/DemoejemploduocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejemplo.demoejemploduoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoejemploduocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
